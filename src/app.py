4b7a27854d2ebf9065d795b7e0fdbdd9213fcb0b
Janmejay Buranpuri <jburanpu@uwo.ca>
2024-04-19 14:06:21 -0400

    test

--- src/app.py
+++ src/app.py
@@ -4,6 +4,7 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def main():
     return '''
@@ -13,7 +14,8 @@ def main():
      </form>
      '''
 
+
 @app.route("/echo_user_input", methods=["POST"])
 def echo_input():
     input_text = request.form.get("user_input", "")
-    return "You entered: " + input_text 
+    return "You entered: " + input_text
